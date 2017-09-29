bdeaf8fcd50e7b26e866f1630bf73875c14ed232
Evtuhov <Evtuhov@binbank.ru>
2017-09-29 07:57:41 +0300

    iteration 1

---
+++ Features/Features/AdvisorViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AdvisorViewController.swift
+//  Features
+//
+//  Created by Aleksander Evtuhov on 29/09/2017.
+//  Copyright © 2017 Aleksander Evtuhov. All rights reserved.
+//
+
+import UIKit
+
+
+class AdvisorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
